5ea61f8715e506ec5f7d25b8b5f056bac90881a0
ilya <da.tot.samy.ilya@gmail.com>
2023-02-21 20:34:07 +0500

    Bannikov Ilya exercises01

--- exercises01/src/main/scala/exercises01/Vector.scala
+++ exercises01/src/main/scala/exercises01/Vector.scala
@@ -1,28 +1,39 @@
 package exercises01
 
 class Vector(val x: Double, val y: Double) {
-  def +(other: Vector): Vector = ???
+  def +(other: Vector): Vector = new Vector (other.x + this.x, other.y + this.y)
 
-  def -(other: Vector): Vector = ???
+  def -(other: Vector): Vector = new Vector (this.x - other.x, this.y - other.y)
 
-  def *(scalar: Double): Vector = ???
+  def *(scalar: Double): Vector = new Vector (scalar*this.x, scalar*this.y)
 
-  def unary_- : Vector = ???
+  def unary_- : Vector = new Vector (-this.x, -this.y)
 
-  def euclideanLength: Double = ???
+  def euclideanLength: Double = Math.sqrt(this.x*this.x + this.y*this.y)
 
-  def normalized: Vector = ???
+  def normalized(): Vector = {
+    if (this.euclideanLength == 0) {
+      return new Vector(0,0)
+    }
+    return new Vector (this.x/this.euclideanLength, this.y/this.euclideanLength)
+  }
 
-  override def equals(other: Any): Boolean = ???
+  override def equals(other: Any): Boolean = this.toString == other.toString
 
   // Vector(x, y)
-  override def toString: String = ???
+  override def toString: String = "Vector(" + this.x + ", " + this.y + ")"
 }
 
 object Vector {
-  def fromAngle(angle: Double, length: Double): Vector = ???
+  def fromAngle(angle: Double, length: Double): Vector = new Vector(length*Math.sin(Math.PI/2-angle), length*Math.sin(angle))
 
-  def sum(list: List[Vector]): Vector = ???
+  def sum(list: List[Vector]): Vector = {
+    var vector = new Vector(0, 0)
+    for (i <- list) {
+      vector += i
+    }
+    return vector
+  }
 
-  def unapply(arg: Vector): Option[(Double, Double)] = ???
+  def unapply(arg: Vector): Option[(Double, Double)] = Option(arg.x, arg.y)
 }
