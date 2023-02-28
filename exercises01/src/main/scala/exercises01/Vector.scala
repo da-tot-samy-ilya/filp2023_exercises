9283b0083e9af66e619dba6d59cedb7c384ec9df
ilya <da.tot.samy.ilya@gmail.com>
2023-02-28 15:52:41 +0500

    Bannikov Ilya exercises01

--- exercises01/src/main/scala/exercises01/Vector.scala
+++ exercises01/src/main/scala/exercises01/Vector.scala
@@ -1,42 +1,41 @@
 package exercises01
 
 class Vector(val x: Double, val y: Double) {
-  def +(other: Vector): Vector = new Vector(other.x + this.x, other.y + this.y)
+  def +(other: Vector): Vector = new Vector(other.x + x, other.y + y)
 
-  def -(other: Vector): Vector = new Vector(this.x - other.x, this.y - other.y)
+  def -(other: Vector): Vector = new Vector(x - other.x, y - other.y)
 
-  def *(scalar: Double): Vector = new Vector(scalar * this.x, scalar * this.y)
+  def *(scalar: Double): Vector = new Vector(scalar * x, scalar * y)
 
-  def unary_- : Vector = new Vector(-this.x, -this.y)
+  def unary_- : Vector = new Vector(-x, -y)
 
-  def euclideanLength: Double = Math.sqrt(this.x * this.x + this.y * this.y)
+  def euclideanLength: Double = Math.sqrt(x * x + y * y)
 
   def normalized(): Vector = {
-    val len = this.euclideanLength
+    val len = euclideanLength
     if (len == 0) {
       return new Vector(0, 0)
     }
-    return new Vector(this.x / len, this.y / len)
+    new Vector(x / len, y / len)
   }
 
   override def equals(other: Any): Boolean = other match {
-    case Vector(this.x, this.y) => true
-    case _                      => false
+    case Vector(x, y) => x == this.x && y == this.y
+    case _            => false
   }
 
   // Vector(x, y)
-  override def toString: String = s"Vector(${this.x}, ${this.y})"
+  override def toString: String = s"Vector(${x}, ${y})"
 }
 
 object Vector {
-
   private val nullVector = new Vector(0, 0)
   def fromAngle(angle: Double, length: Double): Vector =
     new Vector(length * Math.sin(Math.PI / 2 - angle), length * Math.sin(angle))
 
   def sum(list: List[Vector]): Vector = {
-    return list.foldLeft(nullVector)((m, n) => m + n)
+    list.foldLeft(nullVector)(_ + _)
   }
 
-  def unapply(arg: Vector): Option[(Double, Double)] = Option(arg.x, arg.y)
+  def unapply(arg: Vector): Option[(Double, Double)] = Some(arg.x, arg.y)
 }
