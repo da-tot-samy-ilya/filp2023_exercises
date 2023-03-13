1c04181c7e2ca4141cc13569d6a1bfd661544c2e
ilya <da.tot.samy.ilya@gmail.com>
2023-03-13 20:31:07 +0500

    Bannikov Ilya exercises02

--- exercises02/src/main/scala/exercises02/Counter.scala
+++ exercises02/src/main/scala/exercises02/Counter.scala
@@ -2,18 +2,18 @@ package exercises02
 
 object Counter {
 
-  private val re1    = "[()\\s.,!?:\\n\\t\\r]".r
-  private val re2    = "[()\\s!?:\\n\\t\\r]".r
-  private val letter = "[a-zA-Z]".r
-  private val number = "\\d".r
+  private val dividersForWords   = "[()\\s.,!?:\\n\\t\\r]".r
+  private val dividersForNumbers = "[()\\s!?:\\n\\t\\r]".r
+  private val letter             = "[a-zA-Z]".r
+  private val number             = "\\d".r
 
   /**
     * Посчитать количество вхождений слов в тексте
     * слово отделено символами [\s.,!?:\n\t\r]
     */
   def countWords(text: String): Map[String, Int] =
-    text
-      .split(re1.regex)
+    dividersForWords
+      .split(text)
       .filter(_.nonEmpty)
       .map(_.toLowerCase())
       .groupBy(identity)
@@ -24,16 +24,16 @@ object Counter {
     * слово отделено символами [\s.,!?:\n\t\r]
     */
   def countEnglishWords(text: String): Map[String, Int] =
-    countWords(text).filter(item => letter.matches(item._1.slice(0, 1)))
+    countWords(text).filter(item => letter.matches(item._1.charAt(0).toString))
 
   /**
     * Посчитать количество вхождений чисел в тексте
     * число отделено символами [\s!?:\n\t\r]
     */
   def countNumbers(text: String): Map[String, Int] =
-    text
-      .split(re2.regex)
-      .filter(s => s.nonEmpty && number.matches(s.slice(0, 1)))
+    dividersForNumbers
+      .split(text)
+      .filter(s => s.nonEmpty && number.matches(s.charAt(0).toString))
       .groupBy(identity)
       .map(t => (t._1, t._2.length))
 }
