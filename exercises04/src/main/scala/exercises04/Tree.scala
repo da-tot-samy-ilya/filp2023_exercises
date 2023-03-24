14c9e9c167208e3a80101e75cfe416f650248783
ilya <da.tot.samy.ilya@gmail.com>
2023-03-24 22:39:51 +0500

    Bannikov Ilya exercises04

--- exercises04/src/main/scala/exercises04/Tree.scala
+++ exercises04/src/main/scala/exercises04/Tree.scala
@@ -1,34 +1,27 @@
 package exercises04
 
+import scala.annotation.tailrec
+
 sealed trait Tree[+A]
 final case class Leaf[A](value: A)                        extends Tree[A]
 final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 // Необходимо реализовать операции на бинарном дереве
 object Tree {
-  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
-    case Leaf(value)         => f(value)
-    case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
-  }
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = {
+    @tailrec
+    def helper(list: List[Tree[A]], acc: List[B]): List[B] = list match {
+      case Nil                         => acc
+      case Leaf(value) :: tail         => helper(tail, f(value) :: acc)
+      case Branch(left, right) :: tail => helper(left :: right :: tail, acc)
+    }
 
-  def size[A](t: Tree[A]): Int = t match {
-    case Leaf(_)             => 1
-    case Branch(left, right) => 1 + size(left) + size(right)
+    helper(List(t), Nil).reverse.reduce(g)
   }
+  def size[A](t: Tree[A]): Int = fold(t)(_ => 1)(1 + _ + _)
 
-  def max(t: Tree[Int], max_value: Int = Int.MinValue): Int = t match {
-    case Leaf(value)         => Math.max(value, max_value)
-    case Branch(left, right) => Math.max(max(left), max(right))
-  }
+  def max(t: Tree[Int]): Int = fold(t)(identity)(Math.max)
 
-  def depth[A](t: Tree[A]): Int = t match {
-    case Leaf(_)             => 1
-    case Branch(left, right) => 1 + Math.max(depth(left), depth(right))
-  }
-
-  // тут может пригодиться явное указание типа
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
-    case Leaf(value)         => Leaf(f(value))
-    case Branch(left, right) => Branch(map(left)(f), map(right)(f))
-  }
+  def depth[A](t: Tree[A]): Int                 = fold(t)(_ => 1)(1 + Math.max(_, _))
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
 }
--- exercises04/src/main/scala/exercises04/calculator/Calculator.scala
+++ exercises04/src/main/scala/exercises04/calculator/Calculator.scala
@@ -7,45 +7,25 @@ class Calculator[T: Integral] {
   def isZero(t: T): Boolean =
     t == implicitly[Integral[T]].zero
 
+  def mul(t1: T, t2: T): Result[T]   = Success(t1 * t2)
+  def div(t1: T, t2: T): Result[T]   = if (isZero(t2)) DivisionByZero else Success(t1 / t2)
+  def plus(t1: T, t2: T): Result[T]  = Success(t1 + t2)
+  def minus(t1: T, t2: T): Result[T] = Success(t1 - t2)
+  def matching(left: Expr[T], right: Expr[T], func: (T, T) => Result[T], calc: Expr[T] => Result[T]): Result[T] =
+    calc(left) match {
+      case Success(value1) =>
+        calc(right) match {
+          case Success(value2) => func(value1, value2)
+          case DivisionByZero  => DivisionByZero
+        }
+      case DivisionByZero => DivisionByZero
+    }
   def calculate(expr: Expr[T]): Result[T] = expr match {
-    case Mul(left, right) =>
-      calculate(left) match {
-        case Success(value1) =>
-          calculate(right) match {
-            case Success(value2) => Success(value1 * value2)
-            case DivisionByZero  => DivisionByZero
-          }
-        case DivisionByZero => DivisionByZero
-      }
-    case Div(left, right) =>
-      calculate(left) match {
-        case Success(value1) =>
-          calculate(right) match {
-            case Success(value2) =>
-              if (isZero(value2)) DivisionByZero else Success(value1 / value2)
-            case DivisionByZero => DivisionByZero
-          }
-        case DivisionByZero => DivisionByZero
-      }
-    case Plus(left, right) =>
-      calculate(left) match {
-        case Success(value1) =>
-          calculate(right) match {
-            case Success(value2) => Success(value1 + value2)
-            case DivisionByZero  => DivisionByZero
-          }
-        case DivisionByZero => DivisionByZero
-      }
-    case Minus(left, right) =>
-      calculate(left) match {
-        case Success(value1) =>
-          calculate(right) match {
-            case Success(value2) => Success(value1 - value2)
-            case DivisionByZero  => DivisionByZero
-          }
-        case DivisionByZero => DivisionByZero
-      }
-    case Val(v) => Success(v)
+    case Mul(left, right)   => matching(left, right, mul, calculate)
+    case Div(left, right)   => matching(left, right, div, calculate)
+    case Plus(left, right)  => matching(left, right, plus, calculate)
+    case Minus(left, right) => matching(left, right, minus, calculate)
+    case Val(v)             => Success(v)
     case If(iff, cond, left, right) =>
       calculate(cond) match {
         case Success(value) =>
